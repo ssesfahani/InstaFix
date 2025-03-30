4ac1f24fbe021d3086ffb3ff3423a6aaf0dbd710
Akmal <magchuz@pm.me>
2025-03-30 14:57:03 +0700

    Faster lexer using str.find

--- src/internal/jslex.py
+++ src/internal/jslex.py
@@ -2,21 +2,21 @@ def js_lexer_string(js: str):
     idx = 0
     len_js = len(js)
     while idx < len_js:
-        c = js[idx]
-        if c == "'" or c == '"':
-            start_char = c
-            start_idx = idx
-            idx += 1  # Move past the opening quote
+        idx = js.find('"', idx)
+        if idx == -1:
+            break
+        current_char = js[idx]
+        start_char = current_char
+        start_idx = idx
 
-            while idx < len_js:
-                next_char = js[idx]
-                if next_char == start_char:
-                    idx += 1  # Move past the closing quote
-                    yield (start_idx, idx)
-                    break
-                elif next_char == "\\" and idx + 1 < len_js:
-                    idx += 2  # Skip the escape character and the escaped character
-                else:
-                    idx += 1
-        else:
-            idx += 1
+        idx += 1  # Move past the opening quote
+        while idx < len_js:
+            idx = js.find('"', idx)
+            if idx == -1:
+                break
+            elif js[idx-1] == "\\":
+                idx += 1
+            elif js[idx] == start_char:
+                idx += 1  # Move past the closing quote
+                yield (start_idx, idx)
+                break
