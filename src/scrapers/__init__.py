88d511ca71c295add2197b3c822829c7d34720c2
Akmal <magchuz@pm.me>
2025-04-17 06:32:51 +0700

    Use custom serializer

--- src/scrapers/__init__.py
+++ src/scrapers/__init__.py
@@ -1,10 +1,9 @@
-import msgspec
 from loguru import logger
 
 from cache import post_cache
 from internal.singleflight import Singleflight
 from scrapers.api import get_query_api
-from scrapers.data import Post
+from scrapers.data import Post, deserialize_post, serialize_post
 from scrapers.embed import get_embed
 
 scraper_sf = Singleflight[str, Post | None]()
@@ -13,7 +12,7 @@ scraper_sf = Singleflight[str, Post | None]()
 async def get_post(post_id: str, proxy: str = "") -> Post | None:
     post = post_cache.get(post_id)
     if post:
-        return msgspec.json.decode(post, type=Post)
+        return deserialize_post(post)
 
     return await scraper_sf.do(post_id, _get_post, post_id, proxy)
 
@@ -25,5 +24,5 @@ async def _get_post(post_id: str, proxy: str = "") -> Post | None:
     if not post or post.blocked:
         post = await get_query_api(post_id, proxy)
     if post:
-        post_cache.set(post_id, msgspec.json.encode(post))
+        post_cache.set(post_id, serialize_post(post))
     return post
--- src/scrapers/data.py
+++ src/scrapers/data.py
@@ -1,26 +1,83 @@
 import asyncio
+import dataclasses
+import io
 from typing import List
 
 import aiohttp
-import msgspec
 
 from config import config
 
 proxy_limit = asyncio.Semaphore(50)
 
 
-class Media(msgspec.Struct):
+@dataclasses.dataclass
+class Media:
     url: str
     type: str
 
 
-class Post(msgspec.Struct):
+@dataclasses.dataclass
+class Post:
     timestamp: int
     post_id: str
     username: str
     caption: str
     medias: List[Media]
     blocked: bool
+
+
+def serialize_post(post: Post) -> bytes:
+    ret = bytearray()
+    ret.extend(post.timestamp.to_bytes(8, "little"))
+
+    bs = post.post_id.encode()
+    ret.extend(len(bs).to_bytes(4, "little"))
+    ret.extend(bs)
+
+    bs = post.username.encode()
+    ret.extend(len(bs).to_bytes(4, "little"))
+    ret.extend(bs)
+
+    bs = post.caption.encode()
+    ret.extend(len(bs).to_bytes(4, "little"))
+    ret.extend(bs)
+
+    ret.extend(len(post.medias).to_bytes(4, "little"))
+    for media in post.medias:
+        bs = media.url.encode()
+        ret.extend(len(bs).to_bytes(4, "little"))
+        ret.extend(bs)
+        bs = media.type.encode()
+        ret.extend(len(bs).to_bytes(4, "little"))
+        ret.extend(bs)
+    ret.extend(post.blocked.to_bytes(1, "little"))
+    return ret
+
+
+def deserialize_post(data: bytes) -> Post:
+    buf = io.BytesIO(data)
+    timestamp = int.from_bytes(buf.read(8), "little")
+
+    blen = int.from_bytes(buf.read(4), "little")
+    post_id = buf.read(blen).decode()
+
+    blen = int.from_bytes(buf.read(4), "little")
+    username = buf.read(blen).decode()
+
+    blen = int.from_bytes(buf.read(4), "little")
+    caption = buf.read(blen).decode()
+
+    blen = int.from_bytes(buf.read(4), "little")
+    medias = []
+    for _ in range(blen):
+        blen = int.from_bytes(buf.read(4), "little")
+        media_url = buf.read(blen).decode()
+
+        blen = int.from_bytes(buf.read(4), "little")
+        media_type = buf.read(blen).decode()
+        medias.append(Media(media_url, media_type))
+    blocked = bool(int.from_bytes(buf.read(1), "little"))
+    return Post(timestamp, post_id, username, caption, medias, blocked)
 
 
 # https://github.com/aio-libs/aiohttp/issues/4932#issuecomment-1611759696
